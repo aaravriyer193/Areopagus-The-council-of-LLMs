d4a2c20b499dab53e3861948b904fa8619d758df
Aarav <aaravriyer@gmail.com>
2025-09-20 19:37:37 +0400

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { AgentProvider } from './context/AgentContext';
-import Home from './pages/Home';
+import { AgentProvider } from './context/AgentContext.jsx';
+import Home from './pages/home.jsx';
 
 
 export default function App() {
