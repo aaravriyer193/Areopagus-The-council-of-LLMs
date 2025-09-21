2a6fd3240ae699350567f61537b4114e671b77b3
Aarav <aaravriyer@gmail.com>
2025-09-21 12:37:47 +0400

    Delete src/hooks directory

--- src/hooks/useAgent.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { AgentContext } from '../context/AgentContext';
-
-
-export const useAgent = () => {
-const ctx = useContext(AgentContext);
-if (!ctx) throw new Error('useAgent must be used inside AgentProvider');
-return ctx;
-};
